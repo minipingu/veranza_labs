1466fcd1d8afdfcbb34e249cda0835c31ff3c830
Dede nurhidayat <ranmaniax@gmail.com>
2026-09-09 13:55:31 +0700

    komit masss

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -14,7 +14,17 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Stack(
         children: [
           Image.asset('assets/images/background_login_gradient.png'),
-          Container(),
+          Center(
+            child: Container(
+              width: 343,
+              height: 561,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+                color: Colors.white.withValues(alpha: 0.6),
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+          ),
         ],
       ),
     );
