d79ba6a80060d37ed0a58f44a42ed47c11910667
Dede nurhidayat <ranmaniax@gmail.com>
2026-09-10 13:55:34 +0700

    radius

--- lib/pages/login_screen.dart
+++ lib/pages/login_screen.dart
@@ -14,7 +14,12 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset('assets/images/background_login_gradient.png'),
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/background_login_gradient.png',
+              fit: BoxFit.fill,
+            ),
+          ),
           Center(
             child: Padding(
               padding: const EdgeInsets.only(
@@ -41,12 +46,72 @@ class _LoginScreenState extends State<LoginScreen> {
                         width: 34,
                         height: 34,
                       ),
-                      Text(
-                        'Login',
-                        style: GoogleFonts.inter(
-                          fontWeight: .w700,
-                          fontSize: 34,
-                        ),
+                      Column(
+                        spacing: 12,
+                        children: [
+                          Text(
+                            'Login',
+                            style: GoogleFonts.inter(
+                              fontWeight: .w700,
+                              fontSize: 34,
+                            ),
+                          ),
+                          Text(
+                            'Enter your email and password to log in',
+                            style: GoogleFonts.inter(
+                              fontSize: 12,
+                              color: Color(0xFFEDF1F3),
+                            ),
+                          ),
+                        ],
+                      ),
+                      Column(
+                        spacing: 16,
+                        children: [
+                          Column(
+                            spacing: 6,
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Loisbecket@gmail.com',
+                                  filled: true,
+                                  fillColor: Color.fromARGB(255, 255, 255, 255),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                      color: Color(0xFFEDF1F3),
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 205, 238, 255),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: 'Loisbecket@gmail.com',
+                                  filled: true,
+                                  fillColor: Color.fromARGB(255, 255, 255, 255),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                      color: Color(0xFFEDF1F3),
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                      color: Color.fromARGB(255, 205, 238, 255),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ],
                   ),
